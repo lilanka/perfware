5d066076cdcf8935e3b4634558a726f22ef19de7
lilanka <pathirage.lilanka@gmail.com>
2023-06-05 21:18:45 +0530

    added challenges

--- cpu/instr_decoding.c
+++ cpu/instr_decoding.c
@@ -5,7 +5,7 @@
 typedef uint8_t u8;
 typedef unsigned int u32;
 
-u8 buffer[22];
+static u8 buffer[22];
 
 static void read_file(const char* fname) {
   FILE *ptr;
@@ -37,31 +37,31 @@ static int what_is_w(u8 val) {
   return val & 0x01 ? 1 : 0;
 }
 
-static void REG(u8* upper, int* w) {
-	switch (*upper & 0x38) { // REG 
-	case 0x0: w == 0 ? printf(" al") : printf(" ax"); break;
-	case 0x8: w == 0 ? printf(" cl") : printf(" cx"); break;
-	case 0x10: w == 0 ? printf(" dl") : printf(" dx"); break;
-	case 0x18: w == 0 ? printf(" bl") : printf(" bx"); break;
-	case 0x20: w == 0 ? printf(" ah") : printf(" sp"); break;
-	case 0x28: w == 0 ? printf(" ch") : printf(" bp"); break;
-	case 0x30: w == 0 ? printf(" dh") : printf(" si"); break;
-	default: w == 0 ? printf(" bh") : printf(" di"); break;
+static void REG(u8* lower, int* w) {
+	switch (*lower & 0x38) { // REG 
+	case 0x0: *w == 0 ? printf(" al") : printf(" ax"); break;
+	case 0x8: *w == 0 ? printf(" cl") : printf(" cx"); break;
+	case 0x10: *w == 0 ? printf(" dl") : printf(" dx"); break;
+	case 0x18: *w == 0 ? printf(" bl") : printf(" bx"); break;
+	case 0x20: *w == 0 ? printf(" ah") : printf(" sp"); break;
+	case 0x28: *w == 0 ? printf(" ch") : printf(" bp"); break;
+	case 0x30: *w == 0 ? printf(" dh") : printf(" si"); break;
+	default: *w == 0 ? printf(" bh") : printf(" di"); break;
 	}
 }
 
-static void RM(u8* upper, int* w) {
-	switch (*upper & 0xc0) { // MOD
+static void RM(u8* lower, int* w) {
+	switch (*lower & 0xc0) { // MOD
 	case 0xc0: { // 11 
-		switch (*upper & 0x7) { // R/M
-		case 0x0: w == 0 ? printf(" al") : printf(" ax"); break;
-		case 0x1: w == 0 ? printf(" cl") : printf(" cx"); break;
-		case 0x2: w == 0 ? printf(" dl") : printf(" dx"); break;
-		case 0x3: w == 0 ? printf(" bl") : printf(" bx"); break;
-		case 0x4: w == 0 ? printf(" ah") : printf(" sp"); break;
-		case 0x5: w == 0 ? printf(" ch") : printf(" bp"); break;
-		case 0x6: w == 0 ? printf(" dh") : printf(" si"); break;
-		default: w == 0 ? printf(" bh") : printf(" di"); break;
+		switch (*lower & 0x7) { // R/M
+		case 0x0: *w == 0 ? printf(" al") : printf(" ax"); break;
+		case 0x1: *w == 0 ? printf(" cl") : printf(" cx"); break;
+		case 0x2: *w == 0 ? printf(" dl") : printf(" dx"); break;
+		case 0x3: *w == 0 ? printf(" bl") : printf(" bx"); break;
+		case 0x4: *w == 0 ? printf(" ah") : printf(" sp"); break;
+		case 0x5: *w == 0 ? printf(" ch") : printf(" bp"); break;
+		case 0x6: *w == 0 ? printf(" dh") : printf(" si"); break;
+		default: *w == 0 ? printf(" bh") : printf(" di"); break;
 		} 
 		break;
 	}
@@ -69,28 +69,32 @@ static void RM(u8* upper, int* w) {
 		break;
 	}
 	}
-} 
+}
+
+// Note:
+// MOD Field:		00				01					10	
+//						[ ... ]	[ ... + 8 ] [ ... + 16 ]
 
 static void disassemble() {
 	for (u32 byt = 0; byt < sizeof(buffer); byt += 2) {
-		u8 lower = buffer[byt];
-		u8 upper = buffer[byt + 1];
+		u8 higher = buffer[byt];
+		u8 lower = buffer[byt + 1];
 
-		int w = what_is_w(lower);
-		int d = what_is_d(lower);
+		int w = what_is_w(higher);
+		int d = what_is_d(higher);
 
-		switch (lower & 0xfc) { // Op
+		switch (higher & 0xfc) { // Op
 		case 0x88: printf("mov"); break;
 		}
 
-		if (d == 1) {
-			REG(&upper, &w);
+		if (d == 1) { // Load Op
+			REG(&lower, &w);
 			printf(",");
-			RM(&upper, &w);
-		} else {
-			RM(&upper, &w);
+			RM(&lower, &w);
+		} else { // Store Op
+			RM(&lower, &w);
 			printf(",");
-			REG(&upper, &w);
+			REG(&lower, &w);
 		}
 		printf("\n");
 	} 
